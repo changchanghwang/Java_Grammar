9a1e2ccd2a64e70cf1055fb6dc0fb7230617acad
CH_Hwang <hch950627@naver.com>
2021-07-25 01:18:52 +0900

    correct

--- objectQuiz.java
+++ objectQuiz.java
@@ -1,142 +1,137 @@
-class Human{
-    String man;
+class Human {
+    String name;
     int age;
     int speed;
-    int x,y;
+    int x, y;
 
-
-    void information(String man, int age, int speed, int x, int y){
-        printType();
-        printAge();
-        printSpeed();
-        printLocation();
-        this.man = man;
+    public Human(String name, int age, int speed, int x, int y) {
+        this.name = name;
         this.age = age;
         this.speed = speed;
         this.x = x;
         this.y = y;
     }
-    void walk(int x, int y){
-        printLocation();
-        System.out.println("걷습니다.");
+    public Human(String name, int age, int speed){
+        this(name, age, speed, 0, 0);
+    }
+    public String getLocation(){
+        return"("+x+","+y+")";
+    }
+    protected void printWhoAmI() {
+        System.out.println("My name is " + name + ". " + age + " aged.");
+    }
+}
+
+interface Walkable {
+    void walk(int x, int y);
+}
+
+interface Runnable {
+    void run(int x, int y);
+}
+
+interface Swimmable {
+    void swim(int x, int y);
+}
+
+class GrandParent extends Human implements Walkable {
+    public GrandParent(String name, int age) {
+        super(name, age, 1);
+    }
+
+    @Override
+    public void walk(int x, int y) {
+        printWhoAmI();
+        System.out.println("walk speed: " + speed);
         this.x = x;
         this.y = y;
-        printLocation();
-    }
-    public void printType(){
-        System.out.println("타입 : ["+ man +"]");
-    }
-    public void printAge(){
-        System.out.println("나이 : ["+ age +"]");
-    }
-    public void printSpeed(){
-        System.out.println("기본 속도 : ["+ speed +"]");
-    }
-    public void printLocation(){
-        System.out.println("현재위치 (" +x + ", " + y + ")");
+        System.out.println("Walked to " + getLocation());
     }
 }
 
-class Children extends Human{
-    String man = "child";
-    int age = 15;
-    int speed = 5;
-    int x;
-    int y;
+class Child extends Human implements Walkable, Runnable, Swimmable{
+    public Child(String name, int age) {
+        super(name, age, 5);
+    }
 
-    void information(){
-        printType();
-        printAge();
-        printSpeed();
-        printLocation();
+    @Override
+    public void swim(int x, int y) {
+        printWhoAmI();
+        System.out.println("swimming speed: " + (speed + 1));
+        this.x = x;
+        this.y = y;
+        System.out.println("Swum to " + getLocation());
+    }
 
+    @Override
+    public void run(int x, int y) {
+        printWhoAmI();
+        System.out.println("run speed: " + (speed + +2));
+        this.x = x;
+        this.y = y;
+        System.out.println("Ran to " + getLocation());
     }
-    public void printType(){
-        System.out.println("타입 : ["+ man +"]");
-    }
-    public void printAge(){
-        System.out.println("나이 : ["+ age +"]");
-    }
-    public void printSpeed(){
-        System.out.println("기본 속도 : ["+ speed +"]");
-    }
-    public void printLocation(){
-        System.out.println("현재위치 (" +x + ", " + y + ")");
+
+    @Override
+    public void walk(int x, int y) {
+        printWhoAmI();
+        System.out.println("walk speed: " + speed);
+        this.x = x;
+        this.y = y;
+        System.out.println("Walked to " + getLocation());
     }
 }
 
-class Parent extends Human{
-    String man = "parent";
-    int age = 45;
-    int speed = 3;
-    int x;
-    int y;
+class Parent extends Human implements Walkable, Runnable{
+    public Parent(String name, int age) {
+        super(name, age, 3);
+    }
 
-    void information(){
-        printType();
-        printAge();
-        printSpeed();
-        printLocation();
+    @Override
+    public void run(int x, int y) {
+        printWhoAmI();
+        System.out.println("run speed: " + (speed + 2));
+        this.x = x;
+        this.y = y;
+        System.out.println("Ran to " + getLocation());
     }
-    public void printType(){
-        System.out.println("타입 : ["+ man +"]");
-    }
-    public void printAge(){
-        System.out.println("나이 : ["+ age +"]");
-    }
-    public void printSpeed(){
-        System.out.println("기본 속도 : ["+ speed +"]");
-    }
-    public void printLocation(){
-        System.out.println("현재위치 (" +x + ", " + y + ")");
+
+    @Override
+    public void walk(int x, int y) {
+        printWhoAmI();
+        System.out.println("walk speed: " + speed);
+        this.x = x;
+        this.y = y;
+        System.out.println("Walked to " + getLocation());
     }
 }
 
-class GrandParent extends Human {
-    String man = "GrandParent";
-    int age = 75;
-    int speed = 1;
-    int x;
-    int y;
-
-    void information(){
-        printType();
-        printAge();
-        printSpeed();
-        printLocation();
-    }
-    public void printType(){
-        System.out.println("타입 : ["+ man +"]");
-    }
-    public void printAge(){
-        System.out.println("나이 : ["+ age +"]");
-    }
-    public void printSpeed(){
-        System.out.println("기본 속도 : ["+ speed +"]");
-    }
-    public void printLocation(){
-        System.out.println("현재위치 (" +x + ", " + y + ")");
-    }
-}
 
 public class objectQuiz {
     public static void main(String[] args) {
-        Children children = new Children();
-        System.out.println("자식");
-        children.information();
-        System.out.println("----------------------------------------");
-        Parent parent = new Parent();
-        System.out.println("부모");
-        parent.information();
-        System.out.println("----------------------------------------");
-        GrandParent grandparent = new GrandParent();
-        System.out.println("조부모");
-        grandparent.information();
-        System.out.println("----------------------------------------");
-        Human human = new Human();
-        human.walk(1, 1);
-        children.information();
-        parent.information();
-        grandparent.information();
+        Human grandParent = new GrandParent("할아버지", 70);
+        Human parent = new Parent("엄마", 50);
+        Human child = new Child("나", 20);
+
+        Human[] humans = { grandParent, parent, child };
+        for (Human human : humans) {
+            System.out.println(human.name + ", 나이: " + human.age + ", 속도: " + human.speed + ", 장소: " + human
+                    .getLocation());
+        }
+        System.out.println("<활동 시작>");
+        for (Human human : humans) {
+            if (human instanceof Walkable) {
+                ((Walkable) human).walk(1, 1);
+                System.out.println(" - - - - - - ");
+            }
+            if (human instanceof Runnable) {
+                ((Runnable) human).run(2, 2);
+                System.out.println(" - - - - - - ");
+            }
+            if (human instanceof Swimmable) {
+                ((Swimmable) human).swim(3, -1);
+                System.out.println(" - - - - - - ");
+            }
+        }
     }
 }
